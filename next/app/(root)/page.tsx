115b816c809c129c8138645f1dc030fbe2fe0c51
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2023-10-01 23:55:45 +0600

    Prepared basic layout for main pages

--- next/app/(root)/page.tsx
+++ next/app/(root)/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main>
-      <h1>X-Cited</h1>
-    </main>
+    <>
+      {/* <UserButton afterSignOutUrl="/"/> */}
+
+      <h1 className="head-text text-left">Home</h1>
+    </>
   )
 }
