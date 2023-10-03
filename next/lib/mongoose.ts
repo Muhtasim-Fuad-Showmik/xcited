9317e400e5c068ce9517943f0b25173ca91590cd
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2023-10-03 13:01:49 +0600

    Prepared basic boilerplate for the backend

---
+++ next/lib/mongoose.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+// Check the connection status
+let isConnected = false;
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (!process.env.MONGODB_URL) return console.log("MONGODB_URL not found");
+  if (isConnected) return console.log("Already connected to MongoDB");
+
+  try {
+  } catch (error) {}
+};
