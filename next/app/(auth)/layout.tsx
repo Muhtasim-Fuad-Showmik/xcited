4797de3156656dc2be70af3fd52eff579017ebad
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2023-10-03 11:55:41 +0600

    Applied styling and updated layout for the onboarding form

--- next/app/(auth)/layout.tsx
+++ next/app/(auth)/layout.tsx
@@ -1,27 +1,25 @@
-import { ClerkProvider } from "@clerk/nextjs"
-import { Inter } from "next/font/google"
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
 
-import '../globals.css'
+import "../globals.css";
 
 export const metadata = {
-    title: 'X-Cited',
-    description: 'Get X-cited, Stay excited.'
-}
+  title: "X-Cited",
+  description: "Get X-cited, Stay excited.",
+};
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
-    children
-}: { 
-    children: React.ReactNode
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-    return (
-        <ClerkProvider>
-            <html lang="en">
-                <body className={`${inter.className} bg-dark-1`}>
-                    {children}
-                </body>
-            </html>
-        </ClerkProvider>
-    )
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }
